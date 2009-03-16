f693777b28071369f21bff022f93e0a610f94823
Max Aller <max@max-vm-laptop.(none)>
2009-03-15 19:27:23 -0700

    Moved the user mock to the spec_helper with the shared examples

--- spec/slyncy_spec.rb
+++ spec/slyncy_spec.rb
@@ -3,17 +3,6 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 
 describe Slyncy do
   include Slyncy
-  before(:each) do
-    @user = mock("User")
-    @user.should_receive(:get_pictures).any_number_of_times.with(an_instance_of(Fixnum)).and_return {|a| sleep a/100; ['picture'] * a}
-    @user.should_receive(:get_picture).any_number_of_times.with(an_instance_of(Fixnum)).and_return do |a|
-      if a < 10
-        'picture'
-      else
-        raise "No such picture."
-      end
-    end
-  end
 
   # shared examples in the spec helper
   describe Slyncy::JITJobProcessor do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,18 @@ Spec::Runner.configure do |config|
 end
 
 shared_examples_for "a job processor" do
+  before(:each) do
+    @user = mock("User")
+    @user.should_receive(:get_pictures).any_number_of_times.with(an_instance_of(Fixnum)).and_return {|a| sleep a/100; ['picture'] * a}
+    @user.should_receive(:get_picture).any_number_of_times.with(an_instance_of(Fixnum)).and_return do |a|
+      if a < 10
+        'picture'
+      else
+        raise "No such picture."
+      end
+    end
+  end
+
   describe Slyncy::SlyncyCall do
     it "should allow calls" do
       call = slyncy { @user.get_pictures(10) }
